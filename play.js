af6a4c0ca78a7eaa2683bc3de1f61b5e36b33962
jolson615 <jolson615@gmail.com>
2017-09-21 16:40:27 -0500

    removes solutions

--- play.js
+++ play.js
@@ -4,18 +4,10 @@ function preload() {
 
 function setup() {
   // Set up the area where you'll be executing your code.
-  createCanvas(windowWidth, windowHeight); // These built-in variables let you make the canvas exactly the same size as the user's window.
+  createCanvas(800, 680); // These built-in variables let you make the canvas exactly the same size as the user's window.
 }
-
-var circleX = 50;
-var circleY = 50;
 
 function draw() {
   // p5 Will run these instructions continuously
-  clear();
-  var myPurple = color(152, 10, 232);
-  fill(myPurple);
-  ellipse(circleX, circleY, 80, 80);
-  circleX += 2;
-  circleY += 2;
+
 }
